762e5da4e61a309da227bc16599a56e3f1aa62db
maharjun <arjun210493@gmail.com>
2016-05-26 01:29:13 +0530

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="RepoManagement",
+    version="0.1",
+    packages=['RepoManagement'],
+    author="Arjun Rao",
+    author_email="arjun210493@gmail.com",
+    description="This is an Example Package",
+    license="MIT",
+    keywords="Experiment Repository Management",
+    install_requires=['colorama','pyyaml','gitpython','colorclass','terminaltables'],
+    entry_points={
+        'console_scripts':[
+            'rmanager = RepoManagement.RManager:run_console_main'
+        ]
+    }
+)
